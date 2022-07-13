2575c5fa210cd9606666bdc8b030a65f7e16e81d
Jonas Marty <jonas.marty@konplan.com>
2022-07-13 21:02:07 +0200

    undo some code style changes

--- src/Mova21AppBackend/Controllers/EmailController.cs
+++ src/Mova21AppBackend/Controllers/EmailController.cs
@@ -8,68 +8,66 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 
-namespace Mova21AppBackend.Controllers
+namespace Mova21AppBackend.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class EmailController : Controller
 {
+    private readonly IConfiguration _configuration;
 
-    [ApiController]
-    [Route("api/[controller]")]
-    public class EmailController : Controller
+    public EmailController(IConfiguration configuration)
     {
-        private readonly IConfiguration _configuration;
+        _configuration = configuration;
+    }
 
-        public EmailController(IConfiguration configuration)
+    /// <summary>
+    /// Sends an email to the specified receivers with the given attachment in html format.
+    /// </summary>
+    /// <param name="receivers">One or more email addresses separated by comma ','.</param>
+    /// <param name="subject">The subject of the email.</param>
+    /// <param name="body">The html body of the email.</param>
+    /// <param name="attachment">The attachment to be sent, can be null.</param>
+    /// <returns></returns>
+    [EnableCors("AllowAll")]
+    [HttpPost]
+    public async Task<ActionResult> SendMail([FromForm] string logDbJwtToken, [FromForm] string receivers, [FromForm] string subject, [FromForm] string body, IFormFile? attachment)
+    {
+        if (!IsTokenValid(logDbJwtToken))
         {
-            _configuration = configuration;
+            return Forbid();
         }
 
-        /// <summary>
-        /// Sends an email to the specified receivers with the given attachment in html format.
-        /// </summary>
-        /// <param name="receivers">One or more email addresses separated by comma ','.</param>
-        /// <param name="subject">The subject of the email.</param>
-        /// <param name="body">The html body of the email.</param>
-        /// <param name="attachment">The attachment to be sent, can be null.</param>
-        /// <returns></returns>
-        [EnableCors("AllowAll")]
-        [HttpPost]
-        public async Task<ActionResult> SendMail([FromForm]string logDbJwtToken, [FromForm] string receivers, [FromForm] string subject, [FromForm] string body, IFormFile? attachment)
+        using var smtp = new SmtpClient
         {
-            if (!IsTokenValid(logDbJwtToken))
-            {
-                return Forbid();
-            }
-
-            using var smtp = new SmtpClient
-            {
-                Host = _configuration["Smtp:Host"],
-                Port = Convert.ToInt32(_configuration["Smtp:Port"]),
-                EnableSsl = true,
-                Credentials = new NetworkCredential(_configuration["Smtp:Address"], _configuration["Smtp:Password"])
-            };
-            using var message = new MailMessage(_configuration["Smtp:Address"], receivers)
-            {
-                Subject = subject,
-                Body = body,
-                IsBodyHtml = true,
-
-            };
-
-            if (attachment != null)
-            {
-                message.Attachments.Add(new Attachment(attachment.OpenReadStream(), attachment.FileName));
-            }
-            await smtp.SendMailAsync(message);
-            return Ok();
-        }
-
-        private bool IsTokenValid(string logDbJwtToken)
+            Host = _configuration["Smtp:Host"],
+            Port = Convert.ToInt32(_configuration["Smtp:Port"]),
+            EnableSsl = true,
+            Credentials = new NetworkCredential(_configuration["Smtp:Address"], _configuration["Smtp:Password"])
+        };
+        using var message = new MailMessage(_configuration["Smtp:Address"], receivers)
         {
-            using var client = new HttpClient();
-            var message = new HttpRequestMessage(HttpMethod.Get, _configuration["JwtLogDbValidationUrl"]);
-            message.Headers.Add("Authorization", $"Bearer {logDbJwtToken}");
-            var response = client.Send(message);
+            Subject = subject,
+            Body = body,
+            IsBodyHtml = true,
 
-            return response.IsSuccessStatusCode;
+        };
+
+        if (attachment != null)
+        {
+            message.Attachments.Add(new Attachment(attachment.OpenReadStream(), attachment.FileName));
         }
+        await smtp.SendMailAsync(message);
+        return Ok();
+    }
+
+    private bool IsTokenValid(string logDbJwtToken)
+    {
+        using var client = new HttpClient();
+        var message = new HttpRequestMessage(HttpMethod.Get, _configuration["JwtLogDbValidationUrl"]);
+        message.Headers.Add("Authorization", $"Bearer {logDbJwtToken}");
+        var response = client.Send(message);
+
+        return response.IsSuccessStatusCode;
     }
 }
--- src/Mova21AppBackend/Program.cs
+++ src/Mova21AppBackend/Program.cs
@@ -1,21 +1,19 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
-namespace Mova21AppBackend
+namespace Mova21AppBackend;
+
+public class Program
 {
-
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        CreateHostBuilder(args).Build().Run();
     }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
 }
