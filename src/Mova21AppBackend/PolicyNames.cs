ecfd64c3fd6306681e785eee78a3467dc3e343bb
Jonas Marty <jonas.marty@konplan.com>
2022-07-13 21:02:07 +0200

    undo some code style changes

--- src/Mova21AppBackend/PolicyNames.cs
+++ src/Mova21AppBackend/PolicyNames.cs
@@ -1,11 +1,9 @@
-﻿namespace Mova21AppBackend
-{
+﻿namespace Mova21AppBackend;
 
-    public static class PolicyNames
-    {
-        public const string Activity = nameof(Activity);
-        public const string Bike = nameof(Bike);
-        public const string Weather = nameof(Weather);
-        public const string Email = nameof(Email);
-    }
+public static class PolicyNames
+{
+    public const string Activity = nameof(Activity);
+    public const string Bike = nameof(Bike);
+    public const string Weather = nameof(Weather);
+    public const string Email = nameof(Email);
 }
--- src/Mova21AppBackend/Startup.cs
+++ src/Mova21AppBackend/Startup.cs
@@ -14,158 +14,156 @@ using System.Linq;
 using System.Security.Claims;
 using System.Text.Json.Nodes;
 
-namespace Mova21AppBackend
+namespace Mova21AppBackend;
+
+public class Startup
 {
+    private const string OpenIdConnectPolicyName = "OpenIDConnectPolicy";
 
-    public class Startup
+    public Startup(IWebHostEnvironment env, IConfiguration configuration)
     {
-        private const string OpenIdConnectPolicyName = "OpenIDConnectPolicy";
+        Environment = env;
+        Configuration = configuration;
+    }
 
-        public Startup(IWebHostEnvironment env, IConfiguration configuration)
+    public IConfiguration Configuration { get; }
+    public IWebHostEnvironment Environment { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddAuthentication(options =>
         {
-            Environment = env;
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-        public IWebHostEnvironment Environment { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddAuthentication(options =>
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+            .AddJwtBearer(o =>
             {
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
-                .AddJwtBearer(o =>
+                o.Authority = Configuration["Jwt:Authority"];
+                o.TokenValidationParameters.ValidateAudience = false;
+                o.MetadataAddress = Configuration["Jwt:MetadataAddress"];
+                o.Events = new JwtBearerEvents
                 {
-                    o.Authority = Configuration["Jwt:Authority"];
-                    o.TokenValidationParameters.ValidateAudience = false;
-                    o.MetadataAddress = Configuration["Jwt:MetadataAddress"];
-                    o.Events = new JwtBearerEvents
+                    OnAuthenticationFailed = c =>
                     {
-                        OnAuthenticationFailed = c =>
-                        {
-                            c.NoResult();
+                        c.NoResult();
 
-                            c.Response.StatusCode = 500;
-                            c.Response.ContentType = "text/plain";
-                            if (Environment.IsDevelopment())
-                            {
-                                return c.Response.WriteAsync(c.Exception.ToString());
-                            }
-                            return c.Response.WriteAsync("An error occurred processing your authentication.");
-                        },
-                        OnTokenValidated = async tokenValidationContext =>
+                        c.Response.StatusCode = 500;
+                        c.Response.ContentType = "text/plain";
+                        if (Environment.IsDevelopment())
                         {
-                            if (tokenValidationContext.Principal is null)
-                            {
-                                return;
-                            }
+                            return c.Response.WriteAsync(c.Exception.ToString());
+                        }
+                        return c.Response.WriteAsync("An error occurred processing your authentication.");
+                    },
+                    OnTokenValidated = async tokenValidationContext =>
+                    {
+                        if (tokenValidationContext.Principal is null)
+                        {
+                            return;
+                        }
 
-                            var resourceAccessJson = tokenValidationContext.Principal.Claims
-                                .FirstOrDefault(c => c.Type == "resource_access")?.Value;
-                            if (resourceAccessJson == null)
+                        var resourceAccessJson = tokenValidationContext.Principal.Claims
+                            .FirstOrDefault(c => c.Type == "resource_access")?.Value;
+                        if (resourceAccessJson == null)
+                        {
+                            tokenValidationContext.Principal.AddIdentity(new ClaimsIdentity(new[]
                             {
-                                tokenValidationContext.Principal.AddIdentity(new ClaimsIdentity(new[]
-                                {
                                 new Claim(Claims.ActivityEdit, "false"),
                                 new Claim(Claims.BikeEdit, "false"),
                                 new Claim(Claims.WeatherEdit, "false")
-                                }));
-                            }
-                            else
-                            {
-                                JsonObject obj = JsonNode.Parse(resourceAccessJson) as JsonObject;
+                            }));
+                        }
+                        else
+                        {
+                            JsonObject obj = JsonNode.Parse(resourceAccessJson) as JsonObject;
 
-                                var roles = obj["velo-backend"]["roles"].AsArray().Select(x => x.AsValue().GetValue<string>());
-                                tokenValidationContext.Principal.AddIdentity(new ClaimsIdentity(new[]
-                                {
+                            var roles = obj["velo-backend"]["roles"].AsArray().Select(x => x.AsValue().GetValue<string>());
+                            tokenValidationContext.Principal.AddIdentity(new ClaimsIdentity(new[]
+                            {
                                 new Claim(Claims.ActivityEdit, roles.Contains("activity", StringComparer.InvariantCultureIgnoreCase) ? "true" : "false"),
                                 new Claim(Claims.BikeEdit, roles.Contains("velo", StringComparer.InvariantCultureIgnoreCase) ? "true" : "false"),
                                 new Claim(Claims.WeatherEdit, roles.Contains("wetter", StringComparer.InvariantCultureIgnoreCase) ? "true" : "false")
-                                }));
-                            }
+                            }));
                         }
-                    };
+                    }
+                };
+            });
+
+        services.AddCors(options =>
+        {
+            options.AddPolicy("AllowAll",
+                policy =>
+                {
+                    policy.WithOrigins("*");
                 });
+        });
 
-            services.AddCors(options =>
-            {
-                options.AddPolicy("AllowAll",
-                    policy =>
-                    {
-                        policy.WithOrigins("*");
-                    });
-            });
+        services.AddAuthorization(options =>
+        {
+            options.AddPolicy(PolicyNames.Activity, policy => policy.RequireClaim(Claims.ActivityEdit, "true"));
+            options.AddPolicy(PolicyNames.Bike, policy => policy.RequireClaim(Claims.BikeEdit, "true"));
+            options.AddPolicy(PolicyNames.Weather, policy => policy.RequireClaim(Claims.WeatherEdit, "true"));
+        });
 
-            services.AddAuthorization(options =>
-            {
-                options.AddPolicy(PolicyNames.Activity, policy => policy.RequireClaim(Claims.ActivityEdit, "true"));
-                options.AddPolicy(PolicyNames.Bike, policy => policy.RequireClaim(Claims.BikeEdit, "true"));
-                options.AddPolicy(PolicyNames.Weather, policy => policy.RequireClaim(Claims.WeatherEdit, "true"));
-            });
+        services.AddScoped<IBikeRepository, DirectusBikeRepository>();
+        services.AddScoped<IWeatherRepository, DirectusWeatherRepository>();
+        services.AddScoped<IActivityRepository, DirectusActivityRepository>();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
 
-            services.AddScoped<IBikeRepository, DirectusBikeRepository>();
-            services.AddScoped<IWeatherRepository, DirectusWeatherRepository>();
-            services.AddScoped<IActivityRepository, DirectusActivityRepository>();
-            services.AddEndpointsApiExplorer();
-            services.AddSwaggerGen();
+        services.AddControllersWithViews();
+        // In production, the Angular files will be served from this directory
+        services.AddSpaStaticFiles(configuration =>
+        {
+            configuration.RootPath = "ClientApp/dist";
+        });
+    }
 
-            services.AddControllersWithViews();
-            // In production, the Angular files will be served from this directory
-            services.AddSpaStaticFiles(configuration =>
-            {
-                configuration.RootPath = "ClientApp/dist";
-            });
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Error");
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
+        app.UseStaticFiles();
+        if (!env.IsDevelopment())
         {
+            app.UseSpaStaticFiles();
+        }
+
+        app.UseAuthentication();
+
+        app.UseRouting();
+
+        app.UseCors();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller}/{action=Index}/{id?}").RequireCors(OpenIdConnectPolicyName);
+        });
+
+        app.UseSpa(spa =>
+        {
+            spa.Options.SourcePath = "ClientApp";
+
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                spa.UseAngularCliServer(npmScript: "start");
             }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-            }
-
-            app.UseSwagger();
-            app.UseSwaggerUI();
-
-            app.UseStaticFiles();
-            if (!env.IsDevelopment())
-            {
-                app.UseSpaStaticFiles();
-            }
-
-            app.UseAuthentication();
-
-            app.UseRouting();
-
-            app.UseCors();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller}/{action=Index}/{id?}").RequireCors(OpenIdConnectPolicyName);
-            });
-
-            app.UseSpa(spa =>
-            {
-                spa.Options.SourcePath = "ClientApp";
-
-                if (env.IsDevelopment())
-                {
-                    spa.UseAngularCliServer(npmScript: "start");
-                }
-            });
-        }
+        });
     }
 }
