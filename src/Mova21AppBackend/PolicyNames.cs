b9ee5af2a35172ffe09e1e12af433858eba4312b
zzTriplezz <56280866+zzTriplezz@users.noreply.github.com>
2022-07-13 02:38:24 +0200

    add CORS to email endpoint

--- src/Mova21AppBackend/PolicyNames.cs
+++ src/Mova21AppBackend/PolicyNames.cs
@@ -1,9 +1,11 @@
-﻿namespace Mova21AppBackend;
-
-public static class PolicyNames
+﻿namespace Mova21AppBackend
 {
-    public const string Activity = nameof(Activity);
-    public const string Bike = nameof(Bike);
-    public const string Weather = nameof(Weather);
-    public const string Email = nameof(Email);
+
+    public static class PolicyNames
+    {
+        public const string Activity = nameof(Activity);
+        public const string Bike = nameof(Bike);
+        public const string Weather = nameof(Weather);
+        public const string Email = nameof(Email);
+    }
 }
